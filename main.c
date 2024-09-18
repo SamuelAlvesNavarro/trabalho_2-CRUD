d50fff35437a6ca9b10e71af61cf40dbb0dafd6e
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2024-09-18 18:28:50 -0300

    att

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <ctype.h>
 #include <string.h>
+#include <conio.h>
 
 
 #define MAX 100
@@ -23,20 +24,39 @@ typedef struct alunos
 
 
 
-void create(int totalAlunos){
+void create(){
     Aluno alunos[MAX];
+    int i = 0;
     FILE *arquivo;
     char resp;
-    if((arquivo = fopen("dados_alunos.dat", "wb")) == NULL) {
-        printf ("\n Erro de abertura de arquivo\n");
+    if((arquivo = fopen("dados_alunos.dat", "wb")) == NULL){
+        printf("\n Erro de abertura de arquivo\n");
         exit(1);
     }
-
+	printf("\n        			Maximo de 100 alunos\n\n");
     do{
-     fwrite(&alunos, sizeof(alunos), 1, arquivo);
-     printf("\n Adicionar outro aluno (s/n)?: ");
+        printf("\nNome: ");
+        scanf("%s", alunos[i].nome);
+        printf("\nID: ");
+        scanf("%d", &alunos[i].id);
+        printf("\nIdade: ");
+        scanf("%d", &alunos[i].idade);
+        printf("\nCR: ");
+        scanf("%f", &alunos[i].cr);
+        printf("\nCurso: ");
+        scanf("%s", alunos[i].curso);
+        printf("\nData de Nascimento: ");
+        scanf("%d", &alunos[i].data.dia);
+        scanf("%d", &alunos[i].data.mes);
+        scanf("%d", &alunos[i].data.ano);
+		
+        fwrite(&alunos, sizeof(alunos), 1, arquivo);
+        printf("\n Adicionar outro aluno (s/n)?: ");
+        i++;
      do{
-         resp = toupper(getch());
+     	scanf("%c", &resp);
+         resp = toupper(resp);
+         printf("\n");
      }while(resp != 'S' && resp != 'N');
   }while(resp == 'S');
 
@@ -44,7 +64,6 @@ void create(int totalAlunos){
 }
 
 void exibir(Aluno *alunos, int i){
-    printf("\n%-20s %-6s %-10s %-4s %-10s\n", "Nome", "Curso", "ID", "CR", "Data");
     printf("---------------------------------------------------------------\n");
     printf("%-20s %-6s %-10d %-4.1f %02d/%02d/%04d\n", 
             alunos[i].nome, 
@@ -64,8 +83,9 @@ void read(){
         printf ("\n Erro de abertura de arquivo\n");
         exit(1);
     }
+    printf("\n%-20s %-5s %-10s %-4s %-10s\n", "Nome", "Curso", "ID", "CR", "Data");
     while(fread(&alunos, sizeof(alunos), 1, arquivo) == 1){
-        exibir(alunos, i);
+        exibir(&alunos[i], i);
         i++;
     }
 
@@ -104,14 +124,6 @@ void excluirFisico(){
 }
 
 int main(){
-    Aluno alunos[2] = {
-        {"Joao Silva", "CS", 123, 8.5, 21, {15, 3, 2002}},
-        {"Maria Lima", "ENG", 456, 9.2, 22, {10, 6, 2001}}
-    };
-    for(int i = 0; i < 2; i++)
-        read(alunos, i);
-    
-    system("pause");
-    char c[10];
-    scanf("%s", c);
+    create();
+    read();
 }
